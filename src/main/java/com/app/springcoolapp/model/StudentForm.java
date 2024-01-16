c967bdd24b24dd67cff738d07cb88a456fc5ca91
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2024-01-16 10:54:10 +0600

    changes thymeleaf inputs

--- src/main/java/com/app/springcoolapp/model/StudentForm.java
+++ src/main/java/com/app/springcoolapp/model/StudentForm.java
@@ -1,2 +1,18 @@
-package com.app.springcoolapp.model;public class StudentForm {
+package com.app.springcoolapp.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class StudentForm {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String district;
 }
--- src/main/java/com/app/springcoolapp/res/StudentController.java
+++ src/main/java/com/app/springcoolapp/res/StudentController.java
@@ -1,2 +1,30 @@
-package com.app.springcoolapp.res;public class StudentController {
+package com.app.springcoolapp.res;
+
+import com.app.springcoolapp.model.StudentForm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${districts}")
+    private List<String> districts;
+    @GetMapping("/studentRegistration")
+    public String showForm(Model model){
+        StudentForm studentForm = new StudentForm();
+        model.addAttribute("student", studentForm);
+        model.addAttribute("districts", districts);
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") StudentForm studentForm){
+        return "student-confirmation";
+    }
 }
