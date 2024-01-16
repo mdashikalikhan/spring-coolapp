f4d23639de39884a82dbcd10e955ca304827aaf5
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2024-01-16 10:53:42 +0600

    w

--- src/main/java/com/app/springcoolapp/stereotype/component/UILabels.java
+++ src/main/java/com/app/springcoolapp/stereotype/component/UILabels.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class UILabels {
     public static String LABEL_WELCOME = "MVC Welcome Page";
+
+    public static String STUDENT_FORM_TITLE = "Student Registration Form";
+
+    public static String STUDENT_REGISTATION_COMPLETED = "Student Registration Completed";
 }
