85e035eb9105a61f956525cfff160901532bc8d2
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2024-01-17 15:04:26 +0600

    Spring validator works..

--- src/main/java/com/app/springcoolapp/model/StudentForm.java
+++ src/main/java/com/app/springcoolapp/model/StudentForm.java
@@ -1,18 +1,44 @@
 package com.app.springcoolapp.model;
 
 
+
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @NoArgsConstructor
 @Getter @Setter @ToString
 public class StudentForm {
 
+    @NotNull(message = "First Name can't be null")
+    @NotEmpty(message = "Can't be empty")
+    @Size(min = 2,message = "At least two characters are required.")
+    //@Pattern(regexp = "^[A-Z]+(A-Z|a-z)*+[\\\\s.]*",message = "Name is not correct.")
     private String firstName;
 
+    @NotNull(message = "Last Name can't be null")
+    @NotEmpty(message = "Can't be empty")
     private String lastName;
 
+    @NotNull(message = "District can't be null")
+    @NotEmpty(message = "Can't be empty")
     private String district;
+
+    @NotNull(message = "Favourite Language can't be null")
+    @NotEmpty(message = "Can't be empty")
+    private String favouriteLanguage;
+
+    @NotNull(message = "Operating System can't be empty")
+    @Size(min = 1,message = "At least one operating system must be selected")
+    private List<String> favouriteSystems;
+
+    private List<String> districts;
+
+    private List<String> languages;
+
+    private List<String> operatingSystems;
 }
--- src/main/java/com/app/springcoolapp/res/StudentController.java
+++ src/main/java/com/app/springcoolapp/res/StudentController.java
@@ -1,9 +1,11 @@
 package com.app.springcoolapp.res;
 
 import com.app.springcoolapp.model.StudentForm;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +17,35 @@ public class StudentController {
 
     @Value("${districts}")
     private List<String> districts;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> operatingSystems;
+
     @GetMapping("/studentRegistration")
     public String showForm(Model model){
         StudentForm studentForm = new StudentForm();
+        studentForm.setDistricts(districts);
+        studentForm.setLanguages(languages);
+        studentForm.setOperatingSystems(operatingSystems);
         model.addAttribute("student", studentForm);
-        model.addAttribute("districts", districts);
+        /*model.addAttribute("districts", districts);
+        model.addAttribute("languages", languages);
+        model.addAttribute("operatingSystems", operatingSystems);*/
         return "student-form";
     }
 
     @PostMapping("/processStudentForm")
-    public String processForm(@ModelAttribute("student") StudentForm studentForm){
+    public String processForm(@Valid @ModelAttribute("student") StudentForm studentForm,
+                              BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            studentForm.setDistricts(districts);
+            studentForm.setLanguages(languages);
+            studentForm.setOperatingSystems(operatingSystems);
+            return "student-form";
+        }
         return "student-confirmation";
     }
 }
